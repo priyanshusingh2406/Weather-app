ae989293b09c3165f7dac04573405570d0e3b460
priyanshusingh2406 <priyanshusingh8306@gmail.com>
2024-02-15 23:48:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+const userContainer = document.querySelector(".weather-container");
+const grantAccessContainer = document.querySelector(".grant-location-container");
+const searchForm = document.querySelector("[data-searchform]");
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".user-info-container");
+
+
+let currentTab = userTab;
+const API_KEY = '382ab96492010002990ea4bc9d4c12bf';
+currentTab.classList.add("current-tab");
+
